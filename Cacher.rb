593bd16d9cae8315ffacc4caf4b9c35a6e0cfbd1
«Jeaninne» <bokhan.e@gmail.com>
2012-11-09 20:04:37 +0200

    modified:   Cacher.rb 	modified:   spec/Cacher_spec.rb 	modified:   spec/spec_helper.rb

--- Cacher.rb
+++ Cacher.rb
@@ -1,8 +1,68 @@
 module Cacher
 	class Cache
+		attr_accessor :cch, :size
+		def initialize(c_size)
+			@cch = Hash.new
+			@size = c_size
+		end
+
+		def [](index)
+			@cch[index]
+		end
+
+		def key?(index)
+			@cch.key?(index)
+		end
+
+		def add_to_cache(path_to_file)
+			@cch[path_to_file] = Buffer.new(path_to_file)
+		end
+
+		def delete_from_cache(path_to_file)
+			@cch.delete(path_to_file)
+		end
+
+		def find_by_key(path_to_file)	
+			if (@cch.has_key?(path_to_file)) then return @cch[path_to_file] else return nil end
+		end
+
+		def is_full?
+			@cch.size < @size ? (return false) : (return true)
+		end
+
+		def find_new_access()
+		end
+
+		def find_old_access()
+		end
+
+		def find_rarely_access()
+		end
+
 	end
 
 	class Buffer
+	attr_accessor :created_at, :last_accessed, :accesses_count, :data
+		def initialize(path_to_file)
+			@created_at = Time.new
+#			@last_accessed
+			@accesses_count = 1
+#			@data
+		end
+
+		def rewrite(path_to_file)
+		end
+
+		def change_access_time 
+		end
+		
+	end
+
+
+end
+
+class Cacher::File < File
+	def open(file, mode)
 	end
 
 end
--- spec/Cacher_spec.rb
+++ spec/Cacher_spec.rb
@@ -4,8 +4,67 @@ module Cacher
 	describe Cache do
 		context "initialize" do
 			it "should initialize a cache with given size" do
-			new_cache = Cache.new(5).should be
+			new_cache = Cache.new(5)
+			new_cache.should be #that's not fail:(
+			new_cache.size.should eq(5)
 			end
 		end
+
+		context "add in the cache" do
+			it "should add file in the cache" do
+			new_cache = Cache.new(5)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)		
+			new_cache.key?(path_to_file).should be_true
+			end				
+		end
+
+		context "delete from cache" do
+			it "should delete file of cache" do
+			new_cache = Cache.new(5)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)		
+			new_cache.delete_from_cache(path_to_file)
+			new_cache.key?(path_to_file).should be_false
+			end
+		end
+
+		context "find by key" do
+			it "should check if cache include file" do
+			new_cache = Cache.new(5)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)	
+			new_cache.find_by_key(path_to_file).class.should eq(Buffer)
+			end
+			it "should throw an exception if there is no such file" do
+			new_cache = Cache.new(5)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)
+			path_to_file1 = "../test/new1.txt"	
+			new_cache.find_by_key(path_to_file1).should be_nil
+    			end
+		end
+
+		context "full cache" do
+			it "should return 'true' if the cache is full" do
+			new_cache = Cache.new(3)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)
+			path_to_file = "../test/new1.txt"
+			new_cache.add_to_cache(path_to_file)	
+			path_to_file = "../test/new2.txt"
+			new_cache.add_to_cache(path_to_file)		
+			new_cache.is_full?.should be_true
+			end
+			it "should return 'false' if the cache isn't full" do
+			new_cache = Cache.new(3)
+			path_to_file = "../test/new.txt"
+			new_cache.add_to_cache(path_to_file)
+			path_to_file = "../test/new1.txt"
+			new_cache.add_to_cache(path_to_file)		
+			new_cache.is_full?.should be_false
+			end
+		end
+
 	end
 end
